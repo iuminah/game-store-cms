9954e3ce86a1ba03d933f3396e49b90f5bc9e8ce
Minah <huytq.minah@gmail.com>
2022-12-22 17:30:15 +0700

    init cms

---
+++ src/api/feature-cover/routes/feature-cover.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * feature-cover router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::feature-cover.feature-cover');
---
+++ src/api/feature-cover/services/feature-cover.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * feature-cover service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::feature-cover.feature-cover');
