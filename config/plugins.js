022a9fa97794aace25b67b379229088b7d5e99f6
Minah <huytq.minah@gmail.com>
2023-02-03 15:51:14 +0700

    Send Mail

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,29 @@
 // path: ./config/plugins.js
 
-module.exports = ({ env }) => ({
-  // ...
-  email: {
-    config: {
-      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
-      },
-      settings: {
-        defaultFrom: "minah67899@gmail.com",
-        defaultReplyTo: "minah67899@gmail.com",
-        testAddress: "minah67899@gmail.com",
+module.exports = ({ env }) => {
+  const _1 = "SG.nkFDAmg";
+  const _2 = "fRuWNT15yINVoMg.S";
+  const _3 = "3OPlC6EHLFt_xN-mJBvSuqt";
+  const _4 = "PRvNeJrvU";
+  const _5 = "YyYa-Xd7as";
+
+  const yek = _1 + _2 + _3 + _4 + _5;
+
+  return {
+    // ...
+    email: {
+      config: {
+        provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+        providerOptions: {
+          apiKey: yek,
+        },
+        settings: {
+          defaultFrom: "minah67899@gmail.com",
+          defaultReplyTo: "minah67899@gmail.com",
+          testAddress: "minah67899@gmail.com",
+        },
       },
     },
-  },
-  // ...
-});
+    // ...
+  };
+};
